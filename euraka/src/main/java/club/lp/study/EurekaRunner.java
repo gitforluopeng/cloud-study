0fa145cf0950f40750d17340dedae905cc1c1a4a
lwsoftgitforlp <292873959@qq.com>
2021-07-09 16:00:56 +0800

    文件命名规范

--- euraka/src/main/java/club/lp/study/EurekaRunner.java
+++ euraka/src/main/java/club/lp/study/EurekaRunner.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurakaRunner {
+public class EurekaRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurakaRunner.class, args);
+        SpringApplication.run(EurekaRunner.class, args);
     }
 
 }
